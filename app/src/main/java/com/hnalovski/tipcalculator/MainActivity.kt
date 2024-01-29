8058a490016a7906d066411e3bb4ebe9630964b1
Hristijan Nalovski <nalovski.hristijan@protonmail.com>
2024-01-29 23:21:56 +0100

    Finished TopHeader and started working on MainContent

--- app/src/main/java/com/hnalovski/tipcalculator/MainActivity.kt
+++ app/src/main/java/com/hnalovski/tipcalculator/MainActivity.kt
@@ -3,7 +3,9 @@ package com.hnalovski.tipcalculator
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -15,9 +17,12 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.hnalovski.tipcalculator.ui.theme.TipCalculatorTheme
@@ -27,7 +32,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MyApp {
-
+                TopHeader()
             }
         }
     }
@@ -35,13 +40,42 @@ class MainActivity : ComponentActivity() {
 
 @Preview
 @Composable
-fun TopHeader() {
+fun TopHeader(totalPerPerson: Double = 0.0) {
     Surface(
-        modifier = Modifier.fillMaxWidth()
+        modifier = Modifier
+            .fillMaxWidth()
             .height(150.dp)
-            .clip(shape = RoundedCornerShape(corner = CornerSize(12.dp)))
+            .clip(shape = RoundedCornerShape(corner = CornerSize(12.dp))),
+        color = Color(0xFFE9D7F7)
     ) {
+        Column(
+            modifier = Modifier.padding(15.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            val total = "%.2f".format(totalPerPerson)
+            Text(text = "Total per person:",
+                style = MaterialTheme.typography.headlineMedium
+            )
+            Text(text = "$$total",
+                style = MaterialTheme.typography.headlineLarge,
+                fontWeight = FontWeight.ExtraBold)
+        }
+    }
+}
+@Preview
+@Composable
+fun MainContent() {
+    Surface(modifier = Modifier
+        .padding(2.dp)
+        .fillMaxWidth(),
+        shape = RoundedCornerShape(corner = CornerSize(8.dp)),
+        border = BorderStroke(width = 1.dp, color = Color.LightGray)
 
+    ) {
+        Column {
+
+        }
     }
 }
 
